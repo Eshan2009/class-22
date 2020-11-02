cee7edc89506cf47aee3fcf9c61e12dbe13c8857
Eshan2009 <72752337+Eshan2009@users.noreply.github.com>
2020-11-02 10:26:44 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# class-22
-bouncing balls
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+
+var engine,world;
+var ground;
+var ground_option;
+var ball,ball_option;
+
+function setup() {
+createCanvas(400,400);
+engine = Engine.create();
+world = engine.world;
+
+ground_option = {
+  isStatic : true
+}
+
+ball_option = {
+  restitution: 1
+}
+
+ground= Bodies.rectangle(200,390,50,50,ground_option);
+
+World.add(world,ground);
+
+ball = Bodies.circle(200, 200, 10,ball_option);
+
+World.add(world,ball);
+
+console.log(ball)
+}
+
+
+function draw() {
+background(0);
+Engine.update(engine);
+rectMode(CENTER);
+rect (ground.position.x,ground.position.y,400,10);
+ellipseMode(RADIUS);
+ellipse (ball.position.x,ball.position.y,10,10);
+//drawSprites();
+
+}
